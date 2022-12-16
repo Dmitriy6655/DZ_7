eb5790a819008952c136da27e02d9787fe61773d
Timofey_Viligdanov <tim_vi_ov@mail.ru>
2022-12-16 14:05:37 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 47.Задайте двумерный массив размером m×n, заполненный случайными 
+//вещественными  числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1  9
+
+using static Common.Helper;
+
+Console.Write("enter the size line (m): ");
+int line = int.Parse(Console.ReadLine());
+Console.Write("enter the size column (n): ");
+int column = int.Parse(Console.ReadLine());
+
+double[,] array = new double[line, column];
+double[,] arrayResult = RandomArrayDouble(array);
+PrintResultDouble(arrayResult);
